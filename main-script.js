194f0dc6a72162b702d6ad0df6206abeb790093d
HenryMikitish <henry.mikitish@gmail.com>
2023-01-23 13:14:26 -0800

    Make the create button reset the grid each time

--- main-script.js
+++ main-script.js
@@ -1,11 +1,20 @@
 const pad = document.querySelector('.padborder');
 const input = document.querySelector('#textbox');
+let clean = true;
 
 create.addEventListener('click', function() {
     let gridSize = Number(input.value);
 
+    if ( clean == true) {clean = false}
+    else if (clean == false) {
+        //let gridRow = document.querySelector('.gridrow');
+        //let gridColumn = document.querySelector('.gridsquare');
+        //gridColumn.replaceChildren();
+        pad.replaceChildren();
+    }
+
     for (i = 0; i < gridSize; i++) {
-        let gridRow= document.createElement('div');
+        let gridRow = document.createElement('div');
         gridRow.classList.add('gridrow');
         pad.appendChild(gridRow);
 
