34bb1bd3d8a0ab6cb43eb18f3a752bd5ebd5ce04
HenryMikitish <henry.mikitish@gmail.com>
2023-01-23 14:21:30 -0800

    Add sketching functionality

--- main-script.js
+++ main-script.js
@@ -6,16 +6,31 @@ create.addEventListener('click', function() {
    
     pad.replaceChildren();
 
-    for (i = 0; i < gridSize; i++) {
-        let gridRow = document.createElement('div');
-        gridRow.classList.add('gridrow');
-        pad.appendChild(gridRow);
+    if (gridSize <= 100 && gridSize >= 2) {
+        for (i = 0; i < gridSize; i++) {
+            let gridRow = document.createElement('div');
+            gridRow.classList.add('gridrow');
+            pad.appendChild(gridRow);
 
-        for (j = 0; j < gridSize; j++) {
-            let gridColumn = document.createElement('div');
-            gridColumn.classList.add('gridsquare');
-            gridRow.appendChild(gridColumn);
+            for (j = 0; j < gridSize; j++) {
+                let gridColumn = document.createElement('div');
+                gridColumn.classList.add('gridsquare');
+                gridRow.appendChild(gridColumn);
+            }
         }
+    }
+
+    else {
+        let warning = document.createElement('div');
+        warning.textContent = 'Please input a number between 2 and 100.'
+        pad.appendChild(warning);
+    }
+
+    let gridSquares = document.querySelectorAll('.gridsquare')
+
+    for (k = 0; k <= gridSquares.length; k++) {
+        gridSquares[k].addEventListener('mouseover', function(e) {
+            e.target.classList.add('green')});
     }
 
 });
