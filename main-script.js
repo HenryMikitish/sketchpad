fc15d5d10f936935754994dfc9a587ee839513aa
HenryMikitish <henry.mikitish@gmail.com>
2023-01-23 12:25:11 -0800

    Set foundation for grid creation process

--- main-script.js
+++ main-script.js
@@ -1,4 +1,22 @@
 const pad = document.querySelector('.padborder');
 const input = document.querySelector('#textbox');
 
-let xy = Number(input.value)
+create.addEventListener('click', function() {
+    //let grid = document.createElement('div');
+    //grid.textContent = `${Number(input.value)}`;
+    let gridSize = Number(input.value);
+    //pad.appendChild(xy);
+
+    for (i = 0; i < gridSize; i++) {
+        let grid = document.createElement('div');
+        grid.classList.add('grid');
+        pad.appendChild(grid);
+
+        for (j = 0; j < gridSize; j++) {
+            let smallgrid = document.createElement('div');
+            smallgrid.classList.add('smallgrid');
+            grid.appendChild(smallgrid);
+        }
+    }
+
+});
